d7d8f6610a8cf0a0d4ce2ae6da3793665de05daf
Ousikai <mtfallsVR@protonmail.com>
2022-02-25 18:29:22 -0500

    [200~💎 Non Fungible Domains

--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,17 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("ninja");
+    const domainContract = await domainContractFactory.deploy("aincrad");
     await domainContract.deployed();
 
     console.log("Contract deployed to:", domainContract.address);
     
     // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-    let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("kirito",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const address = await domainContract.getAddress("mortal");
-    console.log("Owner of domain mortal:", address);
+    const address = await domainContract.getAddress("Kirito");
+    console.log("Owner of domain kirito:", address);
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
